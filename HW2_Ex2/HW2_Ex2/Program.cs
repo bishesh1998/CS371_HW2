0a31b5b4552f141d6492adc7cad8ef86806e74e7
Bishesh Tuladhar <btuladhar22@my.whitworth.edu>
2020-01-09 13:44:58 -0800

    Ex2 Done

---
+++ HW2_Ex2/HW2_Ex2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2_Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio R1 = new Radio();
+            R1.RadioStatus();
+
+            bool on = false;
+            string power;
+
+            Console.Write("Would you like to turn the radio on? (Y/N): ");
+            power = Console.ReadLine();
+            if (power == "Y" || power == "y")
+            {
+                on = true;
+            }
+
+            if (on == false)
+            {
+                R1.RadioStatus();
+            }
+            else
+            {
+                R1.toggle();
+                Console.Beep();
+                R1.RadioStatus();
+            }
+
+            while (on)
+            {
+                Console.WriteLine("*************** Radio Options *********************** ");
+                Console.WriteLine("1. Volume Change");
+                Console.WriteLine("2. Station Change");
+                Console.WriteLine("3. Turn off");
+                Console.Write("Would you like to do? (1, 2, or 3): ");
+                string user_input = Console.ReadLine();
+                int input = Convert.ToInt32(user_input);
+
+                if (input == 1 )
+                {
+                    Console.Write("What would you like to change the volume to? (Between 0 and 12): ");
+                    int vol = Convert.ToInt32(Console.ReadLine());
+                    R1.crank(vol);
+                }
+                else if (input == 2)
+                {
+                    Console.Write("What would you like to change the station to? (Any decimal number from 88.5 to 107.9): ");
+                    float stat = float.Parse(Console.ReadLine());
+                    R1.tune(stat);
+                }
+                else if (input == 3)
+                {
+                    on = false;
+                    R1.toggle();
+                    Console.Beep();
+                }
+
+                R1.RadioStatus();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HW2_Ex2/HW2_Ex2/Radio.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2_Ex2
+{
+    public interface ISwitch
+    {
+        void toggle();
+    }
+
+    public interface ITunable
+    {
+        void tune(float s);
+        void crank(int v);
+    }
+
+    public class Radio : ISwitch, ITunable
+    {
+        private bool on;
+        private float station;
+        private int volume;
+
+        public Radio()
+        {
+            @on = false;
+            station = 91.7F;
+            volume = 5;
+        }
+
+        public void toggle()
+        {
+            if (@on)
+            {
+                @on = false;
+            }
+            else
+            {
+                @on = true;
+            }
+        }
+
+        public void tune(float s)
+        {
+            if (s > 88.5 && s < 107.9)
+            {
+                Station = s;
+            }
+        }
+
+        public void crank(int v)
+        {
+            if (v >= 0 && v <= 12)
+            {
+                Volume = v;
+            }
+        }
+
+        public bool On
+        {
+            get { return @on; }
+            set { @on = value; }
+        }
+
+        public float Station
+        {
+            get { return station; }
+            set
+            {
+                if (value >= 88.5 && value <= 107.9)
+                {
+                    station = value;
+                }
+            }
+        }
+
+        public int Volume
+        {
+            get { return volume; }
+            set
+            {
+                if (value >= 0 && value <= 12)
+                {
+                    volume = value;
+                }
+            }
+        }
+
+        public void RadioStatus()
+        {
+            if (On)
+            {
+                Console.WriteLine("The radio is turned on.");
+                Console.WriteLine("Volume = {0}", Volume);
+                Console.WriteLine("Station = {0}", Station);
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine ("The radio is off.");
+
+            }
+        }
+    }
+}
